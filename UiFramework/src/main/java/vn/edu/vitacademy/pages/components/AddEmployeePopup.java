7daea868604cf578216336fbbf769e201e6dddbe
Tuyendk4 <tuyenluuminh@gmail.com>
2026-06-29 20:57:42 +0700

    Introduce Cucumber

--- UiFramework/src/main/java/vn/edu/vitacademy/pages/components/AddEmployeePopup.java
+++ UiFramework/src/main/java/vn/edu/vitacademy/pages/components/AddEmployeePopup.java
@@ -17,49 +17,49 @@ public class AddEmployeePopup extends BasePage {
   }
 
   @Step("Input first name '{0}' on registration form")
-  public void inputFirstNameOnRegistrationForm(String firstName) {
+  public void inputFirstName(String firstName) {
     webUI.clearText(findTestObject("REGISTRATION_FORM_TXT_FIRST_NAME"), 20);
     webUI.inputText(findTestObject("REGISTRATION_FORM_TXT_FIRST_NAME"), firstName, 10);
     webUI.attachmentScreenshotWhichMarkElement(findTestObject("REGISTRATION_FORM_TXT_FIRST_NAME"));
   }
 
   @Step("Input last name '{0}' on registration form")
-  public void inputLastNameOnRegistrationForm(String lastName) {
+  public void inputLastName(String lastName) {
     webUI.clearText(findTestObject("REGISTRATION_FORM_TXT_LAST_NAME"));
     webUI.inputText(findTestObject("REGISTRATION_FORM_TXT_LAST_NAME"), lastName);
     webUI.attachmentScreenshotWhichMarkElement(findTestObject("REGISTRATION_FORM_TXT_LAST_NAME"));
   }
 
   @Step("Input age '{0}' on registration form")
-  public void inputAgeOnRegistrationForm(String age) {
+  public void inputAge(String age) {
     webUI.clearText(findTestObject("REGISTRATION_FORM_TXT_AGE"));
     webUI.inputText(findTestObject("REGISTRATION_FORM_TXT_AGE"), age);
     webUI.attachmentScreenshotWhichMarkElement(findTestObject("REGISTRATION_FORM_TXT_AGE"));
   }
 
   @Step("Input email '{0}' on registration form")
-  public void inputEmailOnRegistrationForm(String email) {
+  public void inputEmail(String email) {
     webUI.clearText(findTestObject("REGISTRATION_FORM_TXT_EMAIL"));
     webUI.inputText(findTestObject("REGISTRATION_FORM_TXT_EMAIL"), email);
     webUI.attachmentScreenshotWhichMarkElement(findTestObject("REGISTRATION_FORM_TXT_EMAIL"));
   }
 
   @Step("Input salary '{0}' on registration form")
-  public void inputSalaryOnRegistrationForm(String salary) {
+  public void inputSalary(String salary) {
     webUI.clearText(findTestObject("REGISTRATION_FORM_TXT_SALARY"));
     webUI.inputText(findTestObject("REGISTRATION_FORM_TXT_SALARY"), salary);
     webUI.attachmentScreenshotWhichMarkElement(findTestObject("REGISTRATION_FORM_TXT_SALARY"));
   }
 
   @Step("Input department '{0}' on registration form")
-  public void inputDepartmentOnRegistrationForm(String department) {
+  public void inputDepartment(String department) {
     webUI.clearText(findTestObject("REGISTRATION_FORM_TXT_DEPARTMENT"));
     webUI.inputText(findTestObject("REGISTRATION_FORM_TXT_DEPARTMENT"), department);
     webUI.attachmentScreenshotWhichMarkElement(findTestObject("REGISTRATION_FORM_TXT_DEPARTMENT"));
   }
 
   @Step("Click submit button on registration form")
-  public void clickSubmitButtonOnRegistrationForm() {
+  public void clickSubmitButton() {
     webUI.attachmentScreenshotWhichMarkElement(findTestObject("REGISTRATION_FORM_BTN_SUBMIT"));
     webUI.submit(findTestObject("REGISTRATION_FORM_BTN_SUBMIT"));
     webUI.delayInSeconds(1);
